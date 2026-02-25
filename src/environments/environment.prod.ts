97d162e9e341179822108e072b7c8bf7a81db02c
beta7 <3325215+beta7xx@users.noreply.github.com>
2026-02-25 22:24:24 +0700

    Update API_URL for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_URL: 'https://public-pool.inspire9.co.th',
+    API_URL: 'http://122.154.107.141:3334',
     STRATUM_URL: 'localhost:3333'
 };
